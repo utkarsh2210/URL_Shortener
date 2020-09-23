b655a243943beda9a33854b4053f1f6dc53ec201
unknown <utkarshmaheria@gmail.com>
2020-09-23 11:44:16 +0530

    displayed the url from db

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ mongoose.connect('mongodb://localhost/urlShortener', {
 
 app.set('view engine', 'ejs'); 
 app.use(express.urlencoded({ extended: false }));
-app.get('/', (req, res) => { 
-    res.render('index'); 
+
+
+app.get('/', async (req, res) => { 
+    const shortUrls = await ShortUrl.find();
+    res.render('index', {
+        shortUrls: shortUrls
+    }); 
 }); 
 
 app.post('/shortUrls', async (req, res) => {
